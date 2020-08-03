cabd42e9996d47650d38739e0816f8b7d1e4a8a4
Santiago Vasquez <santiagovespinosa@gmail.com>
2020-08-03 02:05:37 -0400

    Update and rename updateUI.test.js to checkForName.test.js

--- src/tests/checkForName.test.js
+++ src/tests/checkForName.test.js
@@ -1,7 +1,7 @@
-import { updateUI } from '../client/js/updateUI'
+import { checkForName } from '../client/js/updateUI'
 
 describe("Testing the Update UI functionality", () => {
     test("Testing the updateUI() function", () => {
-        expect(updateUI).toBeDefined();
+        expect(checkForName).toBeDefined();
     });
 });
