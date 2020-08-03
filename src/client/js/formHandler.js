8b9ba3e1bbe4de97cd2cd9fe4ac783ecbf349839
Santiago Vasquez <santiagovespinosa@gmail.com>
2020-08-03 02:07:20 -0400

    Update formHandler.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,61 +1,39 @@
-// Personal API Key for OpenWeatherMap API
-const baseURL = 'https://api.meaningcloud.com/sentiment-2.1?key=';
-const apiKey = 'd4c79791d2822a545c90415ba7e01868';
-const apiLang = '&lang=en&url=';
+function handleSubmit(event) {
+  event.preventDefault()
 
-//Button to make it all happen
-const generateButton = document.querySelector('#generate');
+  //Check the text in the form field
+  let formText = document.getElementById('name').value;
+  const validUrl = Client.checkForName(formText);
 
-//Async function to make a GET request to the API
-const retrieveData = async (url, apikey, apilang, usertext) => {
-    const response = await fetch(url+apikey+apilang+usertext);
+  console.log("::: Form Submitted :::")
 
-    try {
-        const allData = await response.json();
-        return allData;
-    }
-    catch(error) {
-        console.log("error", error);
-    }
-};
+  //Submit post request if the URL is valid
+  if (validUrl === 0) {
+    console.log('Please enter a valid URL')
+  } else {
+    fetch('http://localhost:8081/test', {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type' : 'application/json',
+      },
+      body: JSON.stringify({url: formText})
+    })
+    .then(res => res.json())
+    .then(function(res) {
+      console.log(res);
+      const polarity = res.score_tag;
+      const agreement = res.agreement;
+      const subjectivity = res.subjectivity;
+      const confidence = res.confidence;
+      const irony = res.irony;
+      document.getElementById('polarity').innerHTML = `Polarity ${polarity}`;
+      document.getElementById('agreement').innerHTML = `Agreement ${agreement}`;
+      document.getElementById('subjectivity').innerHTML = `Subjectivity ${subjectivity}`;
+      document.getElementById('confidence').innerHTML = `Confidence ${confidence}`;
+      document.getElementById('irony').innerHTML = `Irony ${irony}`;
+    })
+  }
+}
 
-//Async function to make a Post request to add the API data
-const postData = async ( url = '', data = {} ) => {
-
-    const response = await fetch(url, {
-    method: 'POST', 
-    credentials: 'same-origin', 
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),     
-  });
-  
-    try {
-      const newData = await response.json();
-      return newData;
-    }
-    catch(error) {
-      console.log("error", error);
-    }
-  };
-
-//Add event listener to button with a callback function when button is clicked
-generateButton.addEventListener('click', clickedButton);
-
-//Call the GET function 
-function clickedButton() {
-    //User input variables
-    const urlInput = document.querySelector('#urlinput').value;
-    retrieveData(baseURL,apiKey,apiLang,urlInput)
-
-    //Chain promise that makes a POST request to add API data and user input
-        .then(function(data) {
-            console.log(data);
-            postData('http://localhost:8085/dataEntry', {agreement: data.agreement, subjectivity: data.subjectivity, confidence: data.confidence, irony: data.irony})
-            //Call function to update the UI
-            Client.updateUI();
-        })      
-};
-
-export { clickedButton }
+export { handleSubmit }
